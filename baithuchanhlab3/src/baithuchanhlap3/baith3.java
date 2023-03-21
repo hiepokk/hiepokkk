f6e752a245b02fe276900f446b11216d64edc7e8
hiepokk <128445806+hiepokk@users.noreply.github.com>
2023-03-21 11:56:10 +0700

    Add files via upload

---
+++ baithuchanhlab3/src/baithuchanhlap3/baith3.java
@@ -0,0 +1,26 @@
+package baithuchanhlap3;
+import java.util.Scanner;
+public class baith3
+
+{
+	
+	class DienTichChuViHT {
+	    public static double tinhDienTich(double r) {
+	        return r * r * 3.14;
+	    }
+	    public static double tinhChuVi(double r){
+	        return r * 2 * 3.14;
+	    }
+	    public static void main(String[] args) {
+	        double r;
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Nhập bán kính hình tròn: ");
+	        r = scanner.nextDouble();
+	        System.out.println("Diện tích hình tròn là: " + tinhDienTich(r));
+	        System.out.println("Chu vi hình tròn là: "+ tinhChuVi(r));
+	        
+	       
+	    }
+	}
+}
+
